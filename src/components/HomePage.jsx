559d36253dc59f14e754555384310788d29049c0
kadriyeg <denizk2407@gmail.com>
2023-03-22 15:56:06 +0300

    homepage is updated

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,16 @@
-import Col from react-bootstrap
-import logo from "../logo-white.png";
+import "../App.css";
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import logo from '../logoWhite.png'
+
 function HomePage(){
+    document.title = "Ana Sayfa";
     return(
         <Container fluid className="home-page-container">
-           <Row>
-           <Col sm={2} md={3} lg={3}>
-          <img src={logo} className="mergen-logo" alt="logo" />
-        </Col>
-        <Col></Col>
+           <Row className="deneme">
+           <Col sm={2} md={3} lg={3}><img src={logo} className="mergen-logo" alt="logo" /></Col>
            </Row>
-
         </Container>
     );
 }
